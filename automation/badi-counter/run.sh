ea7441669d75129c86aa2ef7b128049cbdc91c26
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2020-06-24 10:47:48 +0200

    Add workflow for badi counter

--- automation/badi-counter/run.sh
+++ automation/badi-counter/run.sh
@@ -19,9 +19,9 @@ $DIR/update_database.py $DIR/badi_counter.csv
 
 # 2. fetch new data, update the db
 echo "Fetch new data..."
-$DIR/fetch_from_api.py > temp.csv
-$DIR/update_database.py temp.csv
-# rm temp.csv
+$DIR/fetch_from_api.py > $DIR/temp.csv
+$DIR/update_database.py $DIR/temp.csv
+rm $DIR/temp.csv
 
 # 3. Export the database as csv
 echo "Export database to CSV..."
