2fa605fd8f3e9c661e3a699b6a5f97a1d7986ca8
Stefan Oderbolz <oderbolz@gmail.com>
2021-09-06 14:39:33 +0000

    Use env variable for URL

--- automation/mrz/mrz_patolu.py
+++ automation/mrz/mrz_patolu.py
@@ -4,16 +4,16 @@ import museumplus
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
+search_url = f"{os.getenv('MRZ_BASE_URL')}/ria-ws/application/module/Object/search"
 user = os.getenv('MRZ_USER')
 pw = os.getenv('MRZ_PASS')
-auth = (user, pw)
 
 records = museumplus.search(
-    url='https://mpzurichrietberg.zetcom.com/MpWeb-mpZurichRietberg/ria-ws/application/module/Object/search',
+    url=search_url,
     query='Patolu',
     limit=1,
     offset=0,
-    requests_kwargs={'auth': auth}
+    requests_kwargs={'auth': (user, pw)}
 )
 
 for record in records:
