df937c97adb65e16e4c38e5bc29c75d29b103dd1
Stefan Oderbolz <oderbolz@gmail.com>
2021-09-28 15:24:35 +0000

    Change output in script

--- automation/mrz/prepare_data.sh
+++ automation/mrz/prepare_data.sh
@@ -13,5 +13,5 @@ trap "cleanup" EXIT
 DIR="$(cd "$(dirname "$0")" && pwd)"
 
 # 1. Generate the CSV and download all attachments
-echo "Generate the CSV and download all attachments"
+echo "Generate the CSV"
 python $DIR/download_data_from_museumplus.py -s "Patolu, MAP" > $DIR/mrz_patolu.csv
