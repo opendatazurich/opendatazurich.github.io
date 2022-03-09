e043fe05847e07b9f15cbc103a810f5f14d1302e
Stefan Oderbolz <oderbolz@gmail.com>
2022-03-09 17:04:09 +0100

    Adapt script for multiple datasets

--- automation/ckan_attributes.py
+++ automation/ckan_attributes.py
@@ -2,15 +2,15 @@
 """Extract STZH CKAN attributes to csv
 
 Usage:
-  ckan_attributes.py --dataset <dataset-name> --file <path-to-file> [--no-verify]
+  ckan_attributes.py [--dataset <dataset-name>] [--file <path-to-file>] [--no-verify]
   ckan_attributes.py (-h | --help)
   ckan_attributes.py --version
 
 Options:
   -h, --help                   Show this screen.
   --version                    Show version.
-  -d, --dataset <dataset-name> Name of the dataset to extract the metadata.
-  -f, --file <path-to-file>    Path to CSV file
+  -d, --dataset <dataset-name> Name of the dataset to extract the metadata, if not given read dataset names from stdin.
+  -f, --file <path-to-file>    Path to CSV file, prints to stdout if not specified.
   --no-verify                  Option to disable SSL verification for requests.
 
 """
@@ -36,17 +36,16 @@ __location__ = os.path.realpath(
 load_dotenv(find_dotenv())
 arguments = docopt(__doc__, version='Extract attributes 1.0')
 
-try:
-    BASE_URL = os.getenv('CKAN_BASE_URL')
-    API_KEY = os.getenv('CKAN_API_KEY')
-    ckan = RemoteCKAN(BASE_URL, apikey=API_KEY)
+BASE_URL = os.getenv('CKAN_BASE_URL')
+API_KEY = os.getenv('CKAN_API_KEY')
+ckan = RemoteCKAN(BASE_URL, apikey=API_KEY)
 
-    # CKAN metadata
-    dataset = arguments['--dataset']
-    data = {
-        'id': dataset,
-    }
+
+def extract_attributes(dataset):
     print(f"Extracting metadata from dataset {dataset}.")
+    data = {
+        'id': dataset.strip(),
+    }
     try:
         if arguments['--no-verify']:
             ckan_metadata = ckan.call_action('package_show', data, requests_kwargs={'verify': False})
@@ -69,24 +68,38 @@ try:
             'attribute_desc': attribute[1],
         }
         rows.append(row)
+    return rows
 
-    # save the file
-    with open(arguments['--file'], 'w') as csvfile:
-        field_names = ['dataset', 'attribute_name', 'attribute_tech_name', 'attribute_desc']
-        writer = csv.DictWriter(
-            csvfile,
-            field_names,
-            delimiter=',',
-            quotechar='"',
-            lineterminator='\n',
-            quoting=csv.QUOTE_MINIMAL
-        )
-        writer.writeheader()
-        writer.writerows(rows)
+try:
+    if arguments['--file']:
+        f = open(arguments['--file'], 'w')
+    else:
+        f = sys.stdout
+    field_names = ['dataset', 'attribute_name', 'attribute_tech_name', 'attribute_desc']
+    writer = csv.DictWriter(
+        f,
+        field_names,
+        delimiter=',',
+        quotechar='"',
+        lineterminator='\n',
+        quoting=csv.QUOTE_MINIMAL
+    )
+    writer.writeheader()
 
-    print(f"Saved metadata to {arguments['--file']}.")
+    # CKAN metadata
+    dataset = arguments['--dataset']
+    if dataset:
+        rows = extract_attributes(dataset)
+         writer.writerows(rows)
+    else:
+        for dataset in sys.stdin:
+            rows = extract_attributes(dataset)
+             writer.writerows(rows)
     
 except Exception as e:
     print("Error: %s" % e, file=sys.stderr)
     print(traceback.format_exc(), file=sys.stderr)
     sys.exit(1)
+finally:
+    if arguments['--file']:
+        f.close()
