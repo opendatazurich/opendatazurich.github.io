497a3cd43dcf9898f853e731af60220c59f3df0e
Stefan Oderbolz <oderbolz@gmail.com>
2022-09-07 14:19:43 +0200

    Update ckan_to_dkuel.py

--- automation/ckan_to_dkuel.py
+++ automation/ckan_to_dkuel.py
@@ -85,7 +85,7 @@ def map_metadata_to_datenobjekt(metadata):
         "DOBeschreibung": convert_markdown(metadata["notes"]),
         "DOKategorie": ", ".join([g["title"] for g in metadata["groups"]]),
         "DOOGDRechtsgrundlagen": metadata["legalInformation"],
-        "DORaeumlicheBeziehung0": metadata["spatialRelationship"],
+        "DORaeumlicheBeziehung": metadata["spatialRelationship"],
         "DODienstabteilung": metadata["url"],
         "DODatenlieferant": "SSZ",
         "DODatenlieferantID": "4",
