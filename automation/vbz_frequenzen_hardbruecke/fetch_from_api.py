fa06a9641df2296bc57da6e79db6bfcbe44a52e9
Stefan Oderbolz <oderbolz@gmail.com>
2022-11-15 14:32:10 +0100

    Fetch today and yesterday

--- automation/vbz_frequenzen_hardbruecke/fetch_from_api.py
+++ automation/vbz_frequenzen_hardbruecke/fetch_from_api.py
@@ -4,7 +4,7 @@ import os
 import sys
 import csv
 import traceback
-from datetime import datetime, date, timedelta
+from datetime import datetime, timedelta
 import requests
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
@@ -24,28 +24,30 @@ try:
     writer = csv.DictWriter(sys.stdout, field_names, quoting=csv.QUOTE_NONNUMERIC)
     writer.writeheader()
 
-    start_date = date(2022, 1, 1)
     today = datetime.now().date()
+    total_days = 2
     for loc in locations:
-        cr = s.get(
-            f"https://vbz.diamondreports.ch:8012/api/location/counter/{loc['Name']}",
-            params={
-                'aggregate': 5,
-                'date': today.strftime('%Y%m%d')
-            }
-        )
-        cr.raise_for_status()
-        counter = cr.json()
-        if len(counter['Counters']) == 0:
-            continue
-
-        for obs in counter['Counters'][0]['Counts']:
-            writer.writerow({
-                'In': obs['In'],
-                'Out': obs['Out'],
-                'Timestamp': obs['Timestamp'],
-                'Name': loc['Name']
-            })
+        for day in range(total_days):
+            current_date = (today - timedelta(days=day))
+            cr = s.get(
+                f"https://vbz.diamondreports.ch:8012/api/location/counter/{loc['Name']}",
+                params={
+                    'aggregate': 5,
+                    'date': current_date.strftime('%Y%m%d')
+                }
+            )
+            cr.raise_for_status()
+            counter = cr.json()
+            if len(counter['Counters']) == 0:
+                continue
+    
+            for obs in counter['Counters'][0]['Counts']:
+                writer.writerow({
+                    'In': obs['In'],
+                    'Out': obs['Out'],
+                    'Timestamp': obs['Timestamp'],
+                    'Name': loc['Name']
+                })
 except Exception as e:
     print("Error: %s" % e, file=sys.stderr)
     print(traceback.format_exc(), file=sys.stderr)
