2e0be982bf21817df8bd97acfecc9efdfe1e387c
Stefan Oderbolz <oderbolz@gmail.com>
2023-06-05 14:51:21 +0200

    Map ftp values

--- automation/wapo_wetterstationen/fetch_from_ftp.py
+++ automation/wapo_wetterstationen/fetch_from_ftp.py
@@ -4,6 +4,8 @@ import os
 import sys
 import csv
 from ftplib import FTP
+from datetime import datetime, date, timedelta
+import pytz
 import traceback
 from dotenv import load_dotenv, find_dotenv
 
@@ -36,6 +38,30 @@ stations = [
     },
 ]
 
+
+class NoFileOnFTPServer(Exception):
+    """
+    This error is raised if there is currently on file on the FTP server to be loaded.
+    """
+
+
+def safefloat(s):
+    if not s:
+        return s
+    return float(s)
+
+
+def safeint(s):
+    if not s:
+        return s
+    f = float(s)
+    r = round(f)
+    if f == r:
+        return int(f)
+    else:
+        raise ValueError(f"Can't parse {s} as int without losing precision")
+
+
 def convert_csv_delim(output_path, input_path, input_delim=';', input_encoding='iso-8859-1'):
     with open(input_path, 'r', encoding=input_encoding) as f:
         reader = csv.DictReader(f, delimiter=input_delim)
@@ -49,11 +75,55 @@ def convert_csv_delim(output_path, input_path, input_delim=';', input_encoding='
                 continue
             rows.append(row)
 
-    header = rows[0].keys()
+    header = [
+        'timestamp_utc',
+        'timestamp_cet',
+        'air_temperature',
+        'water_temperature',
+        'wind_gust_max_10min',
+        'wind_speed_avg_10min',
+        'wind_force_avg_10min',
+        'wind_direction',
+        'windchill',
+        'barometric_pressure_qfe',
+        'precipitation',
+        'dew_point',
+        'global_radiation',
+        'humidity',
+        'water_level',
+    ]
     with open(output_path, 'w', newline='', encoding='utf-8') as csv_file:
-        writer = csv.DictWriter(csv_file, fieldnames=header, delimiter=',')
+        writer = csv.DictWriter(csv_file, fieldnames=header, delimiter=',', quoting=csv.QUOTE_MINIMAL)
         writer.writeheader()
-        writer.writerows(rows)
+        for row in rows:
+            zurich_tz = pytz.timezone('Europe/Zurich')
+            row_date = datetime.strptime(row['Datum/Zeit'], '%d.%m.%Y %H:%M:%S')
+            date_cet = zurich_tz.localize(row_date)
+            date_utc = date_cet.astimezone(pytz.utc)
+
+            pressure_qfe = safefloat(row.get('Luftdruck QFE', ''))
+            pressure_qnh = safefloat(row.get('Luftdruck QNH', ''))
+            if not pressure_qfe and pressure_qnh:
+                pressure_qfe = pressure_qnh - 47 # local pressure, QFE = QNH - 47hPa
+
+            mapped_row = {
+                'timestamp_utc': date_utc.isoformat(),
+                'timestamp_cet': date_cet.isoformat(),
+                'air_temperature': safefloat(row.get('Temp.1, 2m', '')),
+                'water_temperature': safefloat(row.get('Temp. Wasser', '')),
+                'wind_gust_max_10min': safefloat(row.get('WG m/s max.', '')),
+                'wind_speed_avg_10min': safefloat(row.get('WG m/s avr.', '')),
+                'wind_force_avg_10min': safefloat(row.get('Umr. Beaufort')),
+                'wind_direction': safeint(row.get('WR vekt.', '')),
+                'windchill': safefloat(row.get('Windchill', '')),
+                'barometric_pressure_qfe': pressure_qfe,
+                'precipitation': safefloat(row.get('Niederschlag', '')),
+                'dew_point': safefloat(row.get('Taupunkt (°C)', row.get('Taupunkt', ''))),
+                'global_radiation': safeint(row.get('Globalstrahlung', '')),
+                'humidity': safefloat(row.get('rel. Feuchte', '')),
+                'water_level': safefloat(row.get('Pegelstand', '')),
+            }
+            writer.writerow(mapped_row)
 
 
 try:
@@ -66,7 +136,7 @@ try:
         ftp.cwd(station['directory'])
         files_in_cwd = [f[0] for f in ftp.mlsd()]
         if station['filename'] not in files_in_cwd:
-            raise Exception(f"File {station['filename']} not on FTP server in path {station['directory']}")
+            raise NoFileOnFTPServer(f"File {station['filename']} not on FTP server in path {station['directory']}")
 
         input_path = os.path.join(__location__, station['filename'])
         with open(input_path, 'wb') as fp:
@@ -79,7 +149,6 @@ try:
         #ftp.delete(station['filename'])
 
     ftp.quit()
-
 
 except Exception as e:
     print("Error: %s" % e, file=sys.stderr)
