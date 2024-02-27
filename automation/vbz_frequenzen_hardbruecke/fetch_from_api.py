e94cba1612b3557cdfadb98f71cf1eda0123171f
Bosshardt Lorenz (SSZ) <lorenz.bosshardt@zuerich.ch>
2024-02-27 16:05:24 +0100

    tryout new api

--- automation/vbz_frequenzen_hardbruecke/fetch_from_api.py
+++ automation/vbz_frequenzen_hardbruecke/fetch_from_api.py
@@ -15,44 +15,53 @@ pw = os.getenv('SSZ_PASS')
 #pw = os.getenv('VBZ_SSZ_PASSWORD')
 
 try:
+
+    payload = {
+        'username':user,  # ENTER THE USERNAME
+        'password': pw,  # ENTER THE PASSWORD
+    }
+
+    bearer = requests.request("POST", url, json=payload)
+    token = bearer.json()['accessToken']
+    print(token)
     # get locations
-    s = requests.Session()
-    s.auth = (user, pw)
-    r = s.get('https://vbz.diamondreports.ch:8012/api/location')
-    r.raise_for_status()
-    locations = r.json()
-
-    field_names = ['In', 'Out', 'Timestamp', 'Name']
-    writer = csv.DictWriter(sys.stdout, field_names, quoting=csv.QUOTE_NONNUMERIC)
-    writer.writeheader()
-
-    today = datetime.now().date()
-    total_days = 3
-    for loc in locations:
-        for day in range(total_days):
-            current_date = (today - timedelta(days=day))
-            cr = s.get(
-                f"https://vbz.diamondreports.ch:8012/api/location/counter/{loc['Name']}",
-                params={
-                    'aggregate': 5,
-                    'date': current_date.strftime('%Y%m%d')
-                }
-            )
-            cr.raise_for_status()
-            counter = cr.json()
-            if len(counter['Counters']) == 0:
-                continue
-    
-            for obs in counter['Counters'][0]['Counts']:
-                writer.writerow({
-                    'In': obs['In'],
-                    'Out': obs['Out'],
-                    'Timestamp': obs['Timestamp'],
-                    'Name': loc['Name']
-                })
-except Exception as e:
-    print("Error: %s" % e, file=sys.stderr)
-    print(traceback.format_exc(), file=sys.stderr)
-    sys.exit(1)
-finally:
-    sys.stdout.flush()
+    # s = requests.Session()
+    # s.auth = (user, pw)
+    # r = s.get('https://vbz.diamondreports.ch:8012/api/location')
+    # r.raise_for_status()
+    # locations = r.json()
+    #
+    # field_names = ['In', 'Out', 'Timestamp', 'Name']
+    # writer = csv.DictWriter(sys.stdout, field_names, quoting=csv.QUOTE_NONNUMERIC)
+    # writer.writeheader()
+#
+#     today = datetime.now().date()
+#     total_days = 3
+#     for loc in locations:
+#         for day in range(total_days):
+#             current_date = (today - timedelta(days=day))
+#             cr = s.get(
+#                 f"https://vbz.diamondreports.ch:8012/api/location/counter/{loc['Name']}",
+#                 params={
+#                     'aggregate': 5,
+#                     'date': current_date.strftime('%Y%m%d')
+#                 }
+#             )
+#             cr.raise_for_status()
+#             counter = cr.json()
+#             if len(counter['Counters']) == 0:
+#                 continue
+#
+#             for obs in counter['Counters'][0]['Counts']:
+#                 writer.writerow({
+#                     'In': obs['In'],
+#                     'Out': obs['Out'],
+#                     'Timestamp': obs['Timestamp'],
+#                     'Name': loc['Name']
+#                 })
+# except Exception as e:
+#     print("Error: %s" % e, file=sys.stderr)
+#     print(traceback.format_exc(), file=sys.stderr)
+#     sys.exit(1)
+# finally:
+#     sys.stdout.flush()
