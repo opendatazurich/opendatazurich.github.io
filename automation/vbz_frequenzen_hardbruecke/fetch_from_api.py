e3f2169ff3b675ba4decce444e62d828e4a7426c
Bosshardt Lorenz (SSZ) <lorenz.bosshardt@zuerich.ch>
2024-03-11 14:47:10 +0100

    bugfix

--- automation/vbz_frequenzen_hardbruecke/fetch_from_api.py
+++ automation/vbz_frequenzen_hardbruecke/fetch_from_api.py
@@ -23,7 +23,7 @@ payload = {
 
 # defining start- end date
 today = datetime.today()
-start_date = today - timedelta(days=3) # look 3 days back > if error occurs and fixed within 3 days, downtime data will be reincluded
+start_date = today - timedelta(days=1) # look 3 days back > if error occurs and fixed within 3 days, downtime data will be reincluded
 start_date = start_date.strftime("%Y%m%d")
 end_date = today + timedelta(days=1) # some date in the future > will include the latest data
 end_date = end_date.strftime("%Y%m%d")
