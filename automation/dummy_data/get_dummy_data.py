229f301725e64ed325c3e2e0b3a13d40c0c34d69
Bosshardt Lorenz (SSZ) <lorenz.bosshardt@zuerich.ch>
2024-03-14 10:19:40 +0100

    added pipeline for dummy dataset on integration for testing reasons

---
+++ automation/dummy_data/get_dummy_data.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""Fetch CSV from CKAN API
+
+Usage:
+  fetch_from_api.py --file <path-to-csv> [--no-verify]
+  fetch_from_api.py (-h | --help)
+  fetch_from_api.py --version
+
+Options:
+  -h, --help                      Show this screen.
+  --version                       Show version.
+  -f, --file <path-to-csv>        Path to CSV file
+  -p, --parquet <path-to-geojson> Path to parquet file
+  --no-verify                     Option to disable SSL verification for reqests.
+
+"""
+
+import pandas as pd
+import os
+from docopt import docopt
+from dotenv import load_dotenv, find_dotenv
+from ckanapi import RemoteCKAN, NotFound
+
+# Load environment variables from a .env file in the project directory
+load_dotenv(find_dotenv())
+
+# Parse command line arguments using docopt > __doc__ references to docstring (special attribute)
+arguments = docopt(__doc__, version='Fetch CSV from CKAN API 1.0')
+#
+# # Get the absolute path of the current working directory
+# __location__ = os.path.realpath(os.getcwd())
+#
+# # Join the current working directory path with a subdirectory named "data"
+# path = os.path.join(__location__, "data")
+#
+
+# Getting dummy_resource.csv vom INT (dataset: dummy_dataset)
+url = "https://data.integ.stadt-zuerich.ch/dataset/dummy_dataset/download/dummy_resource.csv"
+res_pd = pd.read_csv(url)
+
+# saving as csv
+print("Saving csv")
+csv_path = arguments['--file']
+res_pd.to_csv(csv_path, index=False, encoding='utf-8', date_format='%Y-%m-%dT%H:%M:%SZ')
+
+# saving as parquet
+print("saving as parquet")
+parquet_path = arguments['--parquet']
+res_pd.to_parquet(parquet_path, index=False, encoding='utf-8', date_format='%Y-%m-%dT%H:%M:%SZ')
