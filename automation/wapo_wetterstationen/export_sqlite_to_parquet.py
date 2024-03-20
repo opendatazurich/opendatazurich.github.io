f1d220b9c303469c432ca4291d361dd6c74d928f
Bosshardt Lorenz (SSZ) <lorenz.bosshardt@zuerich.ch>
2024-03-20 17:11:34 +0100

    bugfix

--- automation/wapo_wetterstationen/export_sqlite_to_parquet.py
+++ automation/wapo_wetterstationen/export_sqlite_to_parquet.py
@@ -37,32 +37,30 @@ try:
     conn = sqlite3.connect(filename_db)
     c = conn.cursor()
 
-    # dtype_dict = {
-    #             'timestamp_utc': "datetime64[m]",
-    #             'timestamp_cet': "datetime64[m]",
-    #             'air_temperature': float,
-    #             'water_temperature': float,
-    #             'wind_gust_max_10min': float,
-    #             'wind_speed_avg_10min': float,
-    #             'wind_force_avg_10min': float,
-    #             'wind_direction': int,
-    #             'windchill': float,
-    #             'barometric_pressure_qfe': float,
-    #             'precipitation': float,
-    #             'dew_point': float,
-    #             'global_radiation': float,
-    #             'humidity': float,
-    #             'water_level': float,
-    #         }
+    dtype_dict = {
+                'timestamp_utc': "datetime64[m]",
+                'timestamp_cet': "datetime64[m]",
+                'air_temperature': float,
+                'water_temperature': float,
+                'wind_gust_max_10min': float,
+                'wind_speed_avg_10min': float,
+                'wind_force_avg_10min': float,
+                'wind_direction': int,
+                'windchill': float,
+                'barometric_pressure_qfe': float,
+                'precipitation': float,
+                'dew_point': float,
+                'global_radiation': float,
+                'humidity': float,
+                'water_level': float,
+            }
 
-
-    df = pd.read_sql_query('select * from data order by timestamp_utc asc;', con=conn, parse_dates=["timestamp_utc","timestamp_cet"])
+    df = pd.read_sql_query('select * from data order by timestamp_utc asc;', con=conn)
 
     filename_parquet = arguments['--file']
 
     # change type of column
-    # df = df.astype({'In': 'int', 'Out': 'int', 'Name': 'str'})
-    # df["Timestamp"] = pd.to_datetime(df["Timestamp"])
+    df = df.astype(dtype_dict)
 
     df.to_parquet(filename_parquet, index=False)
 
