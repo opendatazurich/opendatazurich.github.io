755ed782d6018f9463cc08a4237fd50e13a98238
Alexander Güntert <alexander.guentert@zuerich.ch>
2024-10-08 17:12:17 +0200

    try no verify

--- automation/dummy_data/get_dummy_data.py
+++ automation/dummy_data/get_dummy_data.py
@@ -22,6 +22,8 @@ from dotenv import load_dotenv, find_dotenv
 import pyarrow
 import fastparquet
 from ckanapi import RemoteCKAN, NotFound
+import ssl  
+ssl._create_default_https_context = ssl._create_unverified_context
 
 # Load environment variables from a .env file in the project directory
 load_dotenv(find_dotenv())
@@ -38,7 +40,7 @@ arguments = docopt(__doc__, version='Fetch CSV from CKAN API 1.0')
 
 # Getting dummy_resource.csv vom INT (dataset: dummy_dataset)
 url = "https://data.integ.stadt-zuerich.ch/dataset/dummy_dataset/download/dummy_resource.csv"
-res_pd = pd.read_csv(url, storage_options={'verify': False})
+res_pd = pd.read_csv(url)
 
 # saving as csv
 print("Saving csv")
