71cebfd0daf3e63a5c40046a8e2c7b682677ed99
Alexander Güntert <alexander.guentert@zuerich.ch>
2026-01-08 14:57:08 +0100

    test on int

---
+++ automation/combine_csv_to_parquet/dataset_metadata.py
@@ -0,0 +1,49 @@
+dataset_metadata = [
+    # Dataset https://data.stadt-zuerich.ch/dataset/sid_dav_verkehrszaehlung_miv_od2031
+    {
+    "dataset_id": "sid_dav_verkehrszaehlung_miv_od2031",
+    "parquet_filename": "sid_dav_verkehrszaehlung_miv_OD2031.parquet",
+    "date_col": "MessungDatZeit",
+    "date_format_input": "%Y-%m-%dT%H:%M:%S",
+    "dtypes": {
+        'MSID': str, 
+        'MSName': str,
+        'ZSID': str,
+        'ZSName': str,
+        'Achse': str,
+        'HNr': str,
+        'Hoehe': str, 
+        'EKoord': "float64",
+        'NKoord': "float64", 
+        'Richtung': str, 
+        'Knummer': "Int64", 
+        'Kname': str, 
+        'AnzDetektoren': "Int64", 
+        'D1ID': str,
+        'D2ID': str,
+        'D3ID': str,
+        'D4ID': str, 
+        'MessungDatZeit': str, 
+        'LieferDat': str, 
+        'AnzFahrzeuge': "Int64",
+        'AnzFahrzeugeStatus': str,
+        },
+    },
+    # Dataset: https://data.stadt-zuerich.ch/dataset/ted_taz_verkehrszaehlungen_werte_fussgaenger_velo
+    {
+        "dataset_id": "ted_taz_verkehrszaehlungen_werte_fussgaenger_velo",
+        "parquet_filename": "verkehrszaehlungen_werte_fussgaenger_velo.parquet",
+        #"date_col": "DATUM",
+        #"date_format_input": "%Y-%m-%dT%H:%M",
+        "dtypes": {
+            'FK_STANDORT': "Int64", 
+            'DATUM': str,
+            'VELO_IN': "Int64",
+            'VELO_OUT': "Int64",
+            'FUSS_IN': "Int64",
+            'FUSS_OUT': "Int64",
+            'OST': "Int64", 
+            'NORD': "Int64",
+        },
+    }
+]
